0c3ccd451b132350e060f1d7244933c8d5db92f9
Yannick Robert <yrobert@apption.com>
2018-03-27 12:56:29 -0400

    added data formatting into global data obj

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
+
+var data = [];
+
+// Get and format the data
 $.ajax({
-    url: "/data.txt",
-    success: (data) => {
-        console.log(data);
+    url: "https://swarm-project.github.io/data.txt",
+    success: (response) => {
+        let lines = response.split('\n');
+        for(let i=0; i<lines.length; i++){
+            let line = lines[i].split(',');
+
+            // Gather the material mixes
+            let materials = [];
+            for(let j=3; j<8; j++){
+                let material = line[j];
+                if(material && material !== ""){
+                    let splitMat = material.split("%");
+
+                    materials.push({
+                        name: splitMat[1].trim().toProperCase(),
+                        percentage: splitMat[0]
+                    });
+                }
+            }
+
+            let obj = {
+                name: line[0].toProperCase(),
+                amount: line[1],
+                materials: materials                
+            }
+
+            data.push(obj);
+        }
     }
 });
+
+
+
+
+
+
+
+
+
+
+String.prototype.toProperCase = function () {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
